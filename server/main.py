ad620e89c4d1aeae47f8626709a59bad8cc84e12
Akash NP <akashnp1998@gmail.com>
2023-03-17 00:34:15 +0530

    fix: handle CORS from FE, allow requests from :5173

--- server/main.py
+++ server/main.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from functions import get_analyzed_data
 from models import SearchUrlModel
 
 app = FastAPI()
 
+allowed_origins = [
+    "http://localhost:5173"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def root():
     return {"message": "BUDS Root"}
 
 @app.post("/analyzer")
-def analyzed_url(url: SearchUrlModel):
-    data = get_analyzed_data(url=url)
+def analyzed_url(payload: SearchUrlModel):
+    search_url = payload.url
+    data = get_analyzed_data(url=search_url)
     return data
