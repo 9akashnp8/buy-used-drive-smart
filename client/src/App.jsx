d9613d3933eda32ec5ed54c51827b7a251086846
Akash NP <akashnp1998@gmail.com>
2023-03-18 10:14:05 +0530

    feat: update results to display data as Chart over table
    Charts help users visualize and understand data better compared to
    basic tables.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,32 +3,26 @@ import { Box } from "@chakra-ui/react"
 
 import Hero from "./components/Hero"
 import SearchForm from "./components/SearchForm"
-import BaseTable from "./components/Table"
+import PriceByYearChart from "./components/PriceByYearChart"
 
 function App() {
   const [data, setData] = useState('');
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="stretch"
-      gap={5}
-      mx='20%'
-      mt={20}
-    >
-      <Hero/>
-      <SearchForm setData={setData}/>
-      {Object.entries(data).map(([metric, result]) => {
-        return (
-          <BaseTable
-            key={metric}
-            metric={metric}
-            result={result}
-          />
-        )
-      })}
-    </Box>
+    <>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="stretch"
+        gap={5}
+        mx='20%'
+        mt={20}
+      >
+        <Hero/>
+        <SearchForm setData={setData}/>
+      </Box>
+      <PriceByYearChart/>
+    </>
   )
 }
 
